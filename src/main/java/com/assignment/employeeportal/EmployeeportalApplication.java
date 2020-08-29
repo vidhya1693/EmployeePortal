80ef15c4e3c6dfe600b9333a2134ed20533056e8
Srividhya <vidhya1693@gmail.com>
2020-08-29 13:57:49 +0530

    Application base code setup

---
+++ src/main/java/com/assignment/employeeportal/EmployeeportalApplication.java
@@ -0,0 +1,14 @@
+package com.assignment.employeeportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeportalApplication.class, args);
+	}
+
+}
+ 
